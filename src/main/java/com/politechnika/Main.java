110ac86daca7278d9dbf542d199ebf112bd76bb5
Bartłomiej Ciesielski <bartekc8@op.pl>
2019-04-14 21:01:43 +0200

    Implement reading and sending measurements.

--- src/main/java/com/politechnika/Main.java
+++ src/main/java/com/politechnika/Main.java
@@ -1,13 +1,27 @@
 package com.politechnika;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.file.AsyncFile;
+import io.vertx.core.file.OpenOptions;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.parsetools.RecordParser;
+import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
 
 
 public class Main extends AbstractVerticle {
+    private List<MutableInt> sentCount = new ArrayList<>();
+    private long startTime;
 
     @Override
     public void start(Future<Void> future) {
@@ -15,26 +29,88 @@ public class Main extends AbstractVerticle {
         options.setKeepAlive(false);
         WebClient client = WebClient.create(vertx, options);
 
-        client.post(config().getInteger("cassandra.port", 9042),
-                    config().getString("hostname", "localhost"),
-                    config().getString("uri", "/measurement"))
-              .sendJsonObject(createJsonObject(), request -> {
-                  if (request.succeeded()) {
+        File[] csvFilesInDirectory = getCsvFilesInDirectory(config().getString("directoryPath", "csvData/"));
 
-                  } else {
+        startTime = System.currentTimeMillis();
+        readAndPostFiles(csvFilesInDirectory, client);
 
-                  }
-              });
+        vertx.setPeriodic(1000, id -> estimatedEfficiency());
     }
 
-    private JsonObject createJsonObject() {
-        return new JsonObject().put("longitude", -91.053794860839844)
-                               .put("latitude", -3.8782415390014648)
-                               .put("time", "20040822194824")
-                               .put("altitude", 1012.7100219726562)
-                               .put("pressure", 1012.71)
-                               .put("co2", 0.000365346)
-                               .put("airDensity", 2.45201e+25)
-                               .put("surfaceTemperature", 293.139);
+    private File[] getCsvFilesInDirectory(String directoryPath) {
+        File directory = new File(directoryPath);
+        return directory.listFiles((dir, name) -> name.endsWith(".csv"));
+    }
+
+    private void readAndPostFiles(File[] csvFiles, WebClient client) {
+        OpenOptions openOptions = getFileOpenOptions();
+
+        for (File csvFile : csvFiles) {
+            vertx.fileSystem()
+                 .open(csvFile.getPath(), openOptions, measurementFileAsyncHandler(client));
+        }
+    }
+
+    private OpenOptions getFileOpenOptions() {
+        return new OpenOptions().setRead(true)
+                                .setWrite(false)
+                                .setCreate(false);
+    }
+
+    private Handler<AsyncResult<AsyncFile>> measurementFileAsyncHandler(WebClient client) {
+        return handler -> {
+            if (handler.succeeded()) {
+                MutableInt count = new MutableInt();
+                sentCount.add(count);
+
+                AsyncFile asyncFile = handler.result();
+
+                RecordParser recordParser = getCsvRecordParser(client, count);
+                asyncFile.handler(recordParser)
+                         .endHandler(v -> asyncFile.close());
+            }
+        };
+    }
+
+    private HttpRequest<Buffer> getPostRequest(WebClient client) {
+        return client.post(config().getInteger("port", 8080),
+                           config().getString("hostname", "localhost"),
+                           config().getString("uri", "/measurement"));
+    }
+
+    private RecordParser getCsvRecordParser(WebClient client, MutableInt count) {
+        HttpRequest<Buffer> postRequest = getPostRequest(client);
+
+        return RecordParser.newDelimited("\n", bufferedLine -> {
+            JsonObject jsonMeasurement = mapToJsonObject(new String(bufferedLine.getBytes(),
+                                                                    StandardCharsets.UTF_8));
+            postRequest.sendJsonObject(jsonMeasurement, request -> {
+                if (request.succeeded()) {
+                    count.value++;
+                }
+            });
+        });
+    }
+
+    private JsonObject mapToJsonObject(String csvDataLine) {
+        String[] splitData = csvDataLine.split(",");
+
+        return new JsonObject().put("longitude", Double.valueOf(splitData[0]))
+                               .put("latitude", Double.valueOf(splitData[1]))
+                               .put("time", splitData[2])
+                               .put("altitude", Double.valueOf(splitData[3]))
+                               .put("pressure", Double.valueOf(splitData[4]))
+                               .put("co2", Double.valueOf(splitData[5]))
+                               .put("airDensity", Double.valueOf(splitData[6]))
+                               .put("surfaceTemperature", Float.valueOf(splitData[7]));
+    }
+
+    private void estimatedEfficiency() {
+        int count = sentCount.stream().mapToInt(mutableInt -> mutableInt.value).sum();
+        double elapsedTimeInSec = (System.currentTimeMillis() - startTime) / 1000.d;
+
+        System.out.println("Sent count: " + count);
+        System.out.println("Estimated time in sec: " + elapsedTimeInSec);
+        System.out.println("Estimated speed json / sec: " + count / elapsedTimeInSec);
     }
 }
---
+++ src/main/java/com/politechnika/MutableInt.java
@@ -0,0 +1,5 @@
+package com.politechnika;
+
+public class MutableInt {
+    public int value;
+}
